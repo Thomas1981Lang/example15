be6300712266911345f9884dedb7cfe4d1584e6f
Thomas1981Lang <34832500+Thomas1981Lang@users.noreply.github.com>
2019-11-09 12:05:00 +0100

    Heroku Änderungen

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
 import datetime
 
 app = Flask(__name__)
@@ -15,7 +15,8 @@ def index():
 @app.route("/about", methods=["GET", "POST"])
 def about():
     if request.method == "GET":
-        return render_template("about.html")
+        user_name = request.cookies.get("user_name")
+        return render_template("about.html", name=user_name)
     elif request.method == "POST":
         contact_name = request.form.get("contact-name")
         contact_email = request.form.get("contact-email")
@@ -25,7 +26,10 @@ def about():
         print(contact_email)
         print(contact_message)
 
-        return render_template("success.html")
+        response = make_response(render_template("success.html"))
+        response.set_cookie("user_name", contact_name)
+
+        return response
 
 
 @app.route("/portfolio")
@@ -33,17 +37,7 @@ def portfolio():
     return render_template("portfolio.html")
 
 
-# @app.route("/contact", methods=["POST"])
-# def contact():
-#     contact_name = request.form.get("contact-name")
-#     contact_email = request.form.get("contact-email")
-#     contact_message = request.form.get("contact-message")
-#
-#     print(contact_name)
-#     print(contact_email)
-#     print(contact_message)
-#
-#     return render_template("success.html")
+
 
 if __name__ == '__main__':
     app.run()
